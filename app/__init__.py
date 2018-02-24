4087312494f4f3488aa17f7d4c2531d7c780e82f
mtianyan <1147727180@qq.com>
2018-02-24 22:03:50 +0800

    完成电影的添加，文件上传，app.config设置上传路径。

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 # _*_ coding: utf-8 _*_
+import os
+
 __author__ = 'mtianyan'
 __date__ = '2017/8/26 17:05'
 from flask import Flask, render_template
@@ -10,6 +12,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:ty158917@139.199.1
 # 如果设置成 True (默认情况)，Flask-SQLAlchemy 将会追踪对象的修改并且发送信号。
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config['SECRET_KEY'] = 'mtianyan_movie'
+app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
+app.config["FC_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/users/")
 app.debug = True
 db = SQLAlchemy(app)
 
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,9 @@
 # _*_ coding: utf-8 _*_
-from wtforms import Label
+import os
+import uuid
 
-from app import db
+from datetime import datetime
+from app import db, app
 
 __author__ = 'mtianyan'
 __date__ = '2017/8/26 17:06'
@@ -11,6 +13,14 @@ from . import admin
 from flask import render_template, redirect, url_for, flash, session, request
 from app.admin.forms import LoginForm, TagForm, MovieForm
 from app.models import Admin, Tag, Movie
+from werkzeug.utils import secure_filename
+
+
+# 修改文件名称
+def change_filename(filename):
+    fileinfo = os.path.splitext(filename)
+    filename = datetime.now().strftime("%Y%m%d%H%M%S") + str(uuid.uuid4().hex) + fileinfo[-1]
+    return filename
 
 
 def admin_login_req(f):
@@ -150,13 +160,44 @@ def tag_del(id=None):
     return redirect(url_for("admin.tag_list", page=1))
 
 
-@admin.route("/movie/add/")
+@admin.route("/movie/add/", methods=["GET", "POST"])
 @admin_login_req
 def movie_add():
     """
     添加电影页面
     """
     form = MovieForm()
+    if form.validate_on_submit():
+        data = form.data
+        file_url = secure_filename(form.url.data.filename)
+        file_logo = secure_filename(form.logo.data.filename)
+        if not os.path.exists(app.config["UP_DIR"]):
+            # 创建一个多级目录
+            os.makedirs(app.config["UP_DIR"])
+            os.chmod(app.config["UP_DIR"], "rw")
+        url = change_filename(file_url)
+        logo = change_filename(file_logo)
+        # 保存
+        form.url.data.save(app.config["UP_DIR"] + url)
+        form.logo.data.save(app.config["UP_DIR"] + logo)
+        # url,logo为上传视频,图片之后获取到的地址
+        movie = Movie(
+            title=data["title"],
+            url=url,
+            info=data["info"],
+            logo=logo,
+            star=int(data["star"]),
+            playnum=0,
+            commentnum=0,
+            tag_id=int(data["tag_id"]),
+            area=data["area"],
+            release_time=data["release_time"],
+            length=data["length"]
+        )
+        db.session.add(movie)
+        db.session.commit()
+        flash("添加电影成功！", "ok")
+        return redirect(url_for('admin.movie_add'))
     return render_template("admin/movie_add.html", form=form)
 
 
