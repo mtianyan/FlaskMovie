f536479af6f23ec96f536e60c0b17cc6e542881c
mtianyan <1147727180@qq.com>
2018-03-01 18:35:36 +0800

    标签筛选-电影分页-电影搜索-搜素分页-电影详情-电影播放(修复用户不存在导致的报错)

--- app/home/forms.py
+++ app/home/forms.py
@@ -214,3 +214,23 @@ class PwdForm(FlaskForm):
             "class": "btn btn-success",
         }
     )
+
+
+class CommentForm(FlaskForm):
+    content = TextAreaField(
+        label="内容",
+        validators=[
+            DataRequired("请输入内容！"),
+        ],
+        description="内容",
+        render_kw={
+            "id": "input_content"
+        }
+    )
+    submit = SubmitField(
+        '提交评论',
+        render_kw={
+            "class": "btn btn-success",
+            "id": "btn-sub"
+        }
+    )
--- app/home/views.py
+++ app/home/views.py
@@ -11,8 +11,8 @@ __date__ = '2017/8/26 17:07'
 import uuid
 from werkzeug.security import generate_password_hash
 from app import db, app
-from home.forms import RegistForm, LoginForm, UserdetailForm, PwdForm
-from app.models import User, Userlog, Preview
+from home.forms import RegistForm, LoginForm, UserdetailForm, PwdForm, CommentForm
+from app.models import User, Userlog, Preview, Tag, Movie, Comment, Moviecol
 from . import home
 from flask import render_template, url_for, redirect, flash, session, request
 
@@ -22,7 +22,8 @@ def change_filename(filename):
     修改文件名称
     """
     fileinfo = os.path.splitext(filename)
-    filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S") + str(uuid.uuid4().hex) + fileinfo[-1]
+    filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S") + \
+               str(uuid.uuid4().hex) + fileinfo[-1]
     return filename
 
 
@@ -49,8 +50,12 @@ def login():
     if form.validate_on_submit():
         data = form.data
         user = User.query.filter_by(name=data["name"]).first()
-        if not user.check_pwd(data["pwd"]):
-            flash("密码错误！", "err")
+        if user:
+            if not user.check_pwd(data["pwd"]):
+                flash("密码错误！", "err")
+                return redirect(url_for("home.login"))
+        else:
+            flash("账户不存在！", "err")
             return redirect(url_for("home.login"))
         session["user"] = user.name
         session["user_id"] = user.id
@@ -166,13 +171,25 @@ def pwd():
     return render_template("home/pwd.html", form=form)
 
 
-@home.route("/comments/")
+@home.route("/comments/<int:page>/")
 @user_login_req
-def comments():
+def comments(page=None):
     """
-    评论记录
+    个人中心评论记录
     """
-    return render_template("home/comments.html")
+    if page is None:
+        page = 1
+    page_data = Comment.query.join(
+        Movie
+    ).join(
+        User
+    ).filter(
+        Movie.id == Comment.movie_id,
+        User.id == session["user_id"]
+    ).order_by(
+        Comment.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    return render_template("home/comments.html", page_data=page_data)
 
 
 @home.route("/loginlog/<int:page>/", methods=["GET"])
@@ -191,22 +208,119 @@ def loginlog(page=None):
     return render_template("home/loginlog.html", page_data=page_data)
 
 
-@home.route("/moviecol/")
+@home.route("/moviecol/add/", methods=["GET"])
 @user_login_req
-def moviecol():
+def moviecol_add():
     """
-    收藏电影
+    添加电影收藏
     """
-    return render_template("home/moviecol.html")
-#
+    uid = request.args.get("uid", "")
+    mid = request.args.get("mid", "")
+    moviecol = Moviecol.query.filter_by(
+        user_id=int(uid),
+        movie_id=int(mid)
+    ).count()
+    # 已收藏
+    if moviecol == 1:
+        data = dict(ok=0)
+    # 未收藏进行收藏
+    if moviecol == 0:
+        moviecol = Moviecol(
+            user_id=int(uid),
+            movie_id=int(mid)
+        )
+        db.session.add(moviecol)
+        db.session.commit()
+        data = dict(ok=1)
+    import json
+    return json.dumps(data)
 
 
-@home.route("/")
-def index():
+@home.route("/moviecol/<int:page>/")
+@user_login_req
+def moviecol(page=None):
     """
-     首页电影列表
+    电影收藏
     """
-    return render_template("home/index.html")
+    if page is None:
+        page = 1
+    page_data = Moviecol.query.join(
+        Movie
+    ).join(
+        User
+    ).filter(
+        Movie.id == Moviecol.movie_id,
+        User.id == session["user_id"]
+    ).order_by(
+        Moviecol.addtime.desc()
+    ).paginate(page=page, per_page=2)
+    return render_template("home/moviecol.html", page_data=page_data)
+
+
+@home.route("/<int:page>/", methods=["GET"])
+@home.route("/", methods=["GET"])
+def index(page=None):
+    """
+    首页电影列表
+    """
+    tags = Tag.query.all()
+    page_data = Movie.query
+    # 标签
+    tid = request.args.get("tid", 0)
+    if int(tid) != 0:
+        page_data = page_data.filter_by(tag_id=int(tid))
+    # 星级
+    star = request.args.get("star", 0)
+    if int(star) != 0:
+        page_data = page_data.filter_by(star=int(star))
+    # 时间
+    time = request.args.get("time", 0)
+    if int(time) != 0:
+        if int(time) == 1:
+            page_data = page_data.order_by(
+                Movie.addtime.desc()
+            )
+        else:
+            page_data = page_data.order_by(
+                Movie.addtime.asc()
+            )
+    # 播放量
+    pm = request.args.get("pm", 0)
+    if int(pm) != 0:
+        if int(pm) == 1:
+            page_data = page_data.order_by(
+                Movie.playnum.desc()
+            )
+        else:
+            page_data = page_data.order_by(
+                Movie.playnum.asc()
+            )
+    # 评论量
+    cm = request.args.get("cm", 0)
+    if int(cm) != 0:
+        if int(cm) == 1:
+            page_data = page_data.order_by(
+                Movie.commentnum.desc()
+            )
+        else:
+            page_data = page_data.order_by(
+                Movie.commentnum.asc()
+            )
+    if page is None:
+        page = 1
+    page_data = page_data.paginate(page=page, per_page=8)
+    p = dict(
+        tid=tid,
+        star=star,
+        time=time,
+        pm=pm,
+        cm=cm,
+    )
+    return render_template(
+        "home/index.html",
+        tags=tags,
+        p=p,
+        page_data=page_data)
 
 
 @home.route("/animation/")
@@ -220,19 +334,65 @@ def animation():
     return render_template("home/animation.html", data=data)
 
 
-@home.route("/search/")
-def search():
+@home.route("/search/<int:page>/")
+def search(page=None):
     """
     搜索
     """
-    return render_template("home/search.html")
+    if page is None:
+        page = 1
+    key = request.args.get("key", "")
+    movie_count = Movie.query.filter(
+        Movie.title.ilike('%' + key + '%')
+    ).count()
+    page_data = Movie.query.filter(
+        Movie.title.ilike('%' + key + '%')
+    ).order_by(
+        Movie.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    page_data.key = key
+    return render_template("home/search.html", movie_count=movie_count, key=key, page_data=page_data)
 
 
-@home.route("/play/")
-def play():
+@home.route("/play/<int:id>/<int:page>/", methods=["GET", "POST"])
+def play(id=None, page=None):
     """
-    播放
+    播放电影
     """
-    return render_template("home/play.html")
+    movie = Movie.query.join(Tag).filter(
+        Tag.id == Movie.tag_id,
+        Movie.id == int(id)
+    ).first_or_404()
 
-
+    if page is None:
+        page = 1
+    page_data = Comment.query.join(
+        Movie
+    ).join(
+        User
+    ).filter(
+        Movie.id == movie.id,
+        User.id == Comment.user_id
+    ).order_by(
+        Comment.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    form = CommentForm()
+    if "user" in session and form.validate_on_submit():
+        data = form.data
+        comment = Comment(
+            content=data["content"],
+            movie_id=movie.id,
+            user_id=session["user_id"]
+        )
+        db.session.add(comment)
+        db.session.commit()
+        movie.commentnum = movie.commentnum + 1
+        db.session.add(movie)
+        db.session.commit()
+        flash("添加评论成功！", "ok")
+        return redirect(url_for('home.play', id=movie.id, page=1))
+    # 放在后面避免添加评论播放量涨2
+    movie.playnum = movie.playnum + 1
+    db.session.add(movie)
+    db.session.commit()
+    return render_template("home/play.html", movie=movie, form=form, page_data=page_data)
